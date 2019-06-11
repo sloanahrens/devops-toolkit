99e27fe8278000ccc3a846b71f08da2ffbadcb89
Sloan Ahrens <sloan.ahrens@gmail.com>
2019-06-11 11:35:05 -0500

    more clean-up

--- django/stockpicker/stockpicker/static/js/picker.js
+++ django/stockpicker/stockpicker/static/js/picker.js
@@ -28,9 +28,9 @@
       legend: { position: "nw" },
       selection: { mode: "x" },
       xaxis: { mode: "time" },
-      grid:  {show: true, margin: 0, borderWidth: 0, margin: 0,
-                labelMargin: 0, axisMargin: 0, minBorderMargin: 0,
-                hoverable: true, autoHighlight: false}
+      grid:  { show: true, margin: 0, borderWidth: 0,
+               labelMargin: 0, axisMargin: 0, minBorderMargin: 0,
+               hoverable: true, autoHighlight: false}
     },
 
     init: function() {
@@ -43,12 +43,10 @@
       self.getRecommendations();
 
       $("body").click(function(e) {
-        // console.log(self.requestInProgress);
         if (!self.requestInProgress) {
           var ticker = $(e.target).attr("data-ticker");
           if (ticker) {
             self.requestInProgress = true;
-            // console.debug(ticker + " clicked");
             self.clearMessages();
             self.getTickerData(ticker, self.indexTicker);
             return false;
@@ -67,7 +65,6 @@
 
       $("#add_ticker_form").submit(function() {
         var ticker = $("#add_ticker_text").val().toUpperCase();
-        // console.debug("add: " + ticker);
         if (ticker && !self.requestInProgress) {
           self.clearMessages();
           self.addNewTicker(ticker);
@@ -76,13 +73,12 @@
       });
 
       $(".unzoom").click(function() {
-          // console.debug("Unzoom click");
+          console.debug('unzoom click');
           self.clearMessages();
           self.plotCharts(self.chartOptions);
       });
 
       $(".chart").on("plotselected", function(ev, ranges) {
-        // console.debug("plotselected");
         self.plotCharts($.extend(true, {}, self.chartOptions,
           { xaxis: { min: ranges.xaxis.from, max: ranges.xaxis.to } }
         ));
@@ -90,15 +86,12 @@
 
       $(".chart").on("plothover", function(ev, pos) {
 
-        //if (ev.currentTarget) console.debug(ev.currentTarget, pos.pageX);
-
         var left = self.chartPlotObjList[0].pointOffset(pos).left;
 
         if (left >= 0 && left <= $(".chart").width()) {
           $(".marker").css({'left':left}).show();
 
           var date = new Date(pos.x);
-          var date_string = [date.getFullYear(),date.getMonth(),date.getDate()].join('-');
 
           var i = self.searchResults.length - 1;
           while (i > 0 && self.searchResults[i].date > date) i--;
@@ -170,7 +163,6 @@
 
       deferredRequest.done(function(json) {
         self.requestInProgress = false;
-        // console.debug(json);
         if (json.success){
           $("#error_message").html("");
           $("#error_message").hide();
@@ -181,7 +173,6 @@
           self.getTickerData(ticker, self.indexTicker);
         }
         else {
-          // console.debug(json.msg);
           $("#success_message").html("");
           $("#success_message").hide();
           $("#error_message").html(json.msg);
@@ -239,7 +230,6 @@
 
       deferredRequest.done(function(json) {
         self.requestInProgress = false;
-        // console.debug(json);
         self.recommendationResults = json;
 
         self.displayRecommendations(json, null);
@@ -255,11 +245,9 @@
           startingLetters[hit['symbol'][0]] = null;
         }
 
-        //console.log(startingLetters);
         var lettersArray = [];
         for(var prop in startingLetters) lettersArray.push(prop);
         lettersArray.sort();
-        //console.log(lettersArray);
         $("#filter_rec").append('<a href="#" data-filter="none">none</a>&nbsp;&nbsp;&nbsp;');
         for(var i=0; i<lettersArray.length; i++){
           $("#filter_rec").append('<a href="#" data-filter="' + lettersArray[i] + '">' + lettersArray[i] + '</a>&nbsp;&nbsp;');
@@ -276,7 +264,6 @@
 
       deferredRequest.done(function(json) {
         self.requestInProgress = false;
-        // console.debug(json);
         $("#tickerlist").html('');
         if (json.tickers.length > 0) {
             var startsWith = json.tickers[0][0];
@@ -298,7 +285,7 @@
       "#984EA3",
       "#000000",
       "#FF7F00",
-      "#FFFF33",
+      "#FFFF33"
     ],
 
 
@@ -310,7 +297,6 @@
       var deferredRequest = this.executeTickerDataRequest(ticker, index);
 
       deferredRequest.done(function(json) {
-        // console.debug(json);
 
         self.dataLength = json.results.length;
 
@@ -340,17 +326,8 @@
             { name: "iac", label: json.index, data: self.dataSeriesDict["iac"] } ],
 
           [ { name: "sac", label: 'Scaled Adj Close', data: self.dataSeriesDict['sac'] },
-            { name: "sac_ma", label: json.avg_weeks + '-wk SAC Mv Avg', data: self.dataSeriesDict['sac_ma'] } ],
-
-          // [ { name: "d_sacma", label: 'Grad SACMA', data: self.dataSeriesDict['d_sacma'] },
-          //   //{ name: "d_sacma_dt", label: 'd_sacma_dt', data: self.dataSeriesDict['d_sacma_dt'] },
-          //   { name: "", label: '0', data: self.dataSeriesDict['zeros'] } ],
-
-          // [ { name: "ac_cr", label: 'AC CC Ret', data: self.dataSeriesDict['ac_cr'] },
-          //   { name: "", label: '0', data: self.dataSeriesDict['zeros'] } ]
+            { name: "sac_ma", label: json.avg_weeks + '-wk SAC Mv Avg', data: self.dataSeriesDict['sac_ma'] } ]
         ];
-
-        // console.log(ticker, self.dataSeriesDict.date.length);
 
         self.plotCharts(self.chartOptions);
 
